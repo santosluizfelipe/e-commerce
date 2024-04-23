a62b0d0b60a5813fd2018c723ce5acd01c6b19e5
Luiz Santos <luizsantos@Luizs-MacBook-Air-2.local>
2024-04-23 16:35:00 +0100

    feat: adding basket to the DB

---
+++ backend/models/basket.js
@@ -0,0 +1,27 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Basket extends Model {
+    static associate(models) {
+      Basket.belongsTo(models.BuyerUser, {
+        foreignKey: 'buyerUserId', 
+        allowNull: false 
+      });
+      Basket.belongsToMany(models.Product, { through: 'BasketProducts' });
+    }
+  }
+
+  Basket.init(
+    {
+      // Define any additional fields you need for the Basket model
+    },
+    {
+      sequelize,
+      modelName: 'Basket',
+      tableName: 'Baskets' 
+    }
+  );
+
+  return Basket;
+};
